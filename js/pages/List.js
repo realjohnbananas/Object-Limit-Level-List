2962689d06b08bc80e541e66746698f1a6d48c34
realjohnbananas <realjohnbananas@gmail.com>
2026-01-08 12:31:44 +1300

    Update List.js
    fixed mb

--- js/pages/List.js
+++ js/pages/List.js
@@ -22,8 +22,6 @@ export default {
         </main>
         <main v-else class="page-list">
             <div class="list-container">
-            <main v-else class="page-list">
-            <div class="list-container">
                     <tr v-for="([level, err], i) in list">
                         <td class="rank">
                             <p v-if="i + 1 <= 150" class="type-label-lg">#{{ i + 1 }}</p>
